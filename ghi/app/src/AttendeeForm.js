a94337ec0452791c351bfd0bbdc230893ae4192b
Matt Snyder <msnyd87@gmail.com>
2023-05-30 21:21:06 -0400

    Attendee and Conference Form added no errors

--- ghi/app/src/AttendeeForm.js
+++ ghi/app/src/AttendeeForm.js
@@ -7,32 +7,43 @@ function AttendeeForm() {
     const [conferences, setConferences] = useState([]);
     const [isSuccess, setIsSuccess] = useState(false);
 
-    useEffect(() => {
-        async function fetchConferences() {
-            const url = 'http://localhost:8000/api/conferences/';
-            const response = await fetch(url);
-            if (response.ok) {
-                const data = await response.json();
-                console.log(data.conferences)
-                setConferences(data.conferences.map(conference => ({
-                    id: conference.id,
-                    name: conference.name,
-                    href: conference.import_href
-                })));
-            }
-        }
-        fetchConferences();
-    }, []);
+    const handleNameChange = (event) => {
+        const value = event.target.value;
+        setName(value);
+    }
+    const handleEmailChange = (event) => {
+        const value = event.target.value;
+        setEmail(value);
+    }
+    const handleConferenceChange = (event) => {
+        const conferenceId = event.target.value;
+        setConference(conferenceId);
+    }
+
+    // useEffect(() => {
+    //     async function fetchConferences() {
+    //         const url = 'http://localhost:8000/api/conferences/';
+    //         const response = await fetch(url);
+    //         if (response.ok) {
+    //             const data = await response.json();
+    //             console.log(data.conferences)
+    //             setConferences(data.conferences.map(conference => ({
+    //                 id: conference.id,
+    //                 name: conference.name,
+    //                 href: conference.import_href
+    //             })));
+    //         }
+    //     }
+    //     fetchConferences();
+    // }, []);
 
     const handleSubmit = async (event) => {
         event.preventDefault();
 
-        console.log("selected conference:", conference);
-
         const data = {};
         data.name = name;
         data.email = email;
-        data.conference = parseInt(conference);
+        data.conference = conference;
         console.log(data);
 
         const attendeeUrl = 'http://localhost:8001/api/attendees/';
@@ -54,6 +65,27 @@ function AttendeeForm() {
             setIsSuccess(true);
         }
     }
+    const fetchData = async () => {
+        const url = 'http://localhost:8000/api/conferences/';
+
+        const response = await fetch(url);
+
+        if (response.ok) {
+            const data = await response.json()
+            setConferences(data.conferences);
+        }
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    let spinnerClasses = 'd-flex justify-content-center mb-3';
+    let dropdownClasses = 'form-select d-none';
+    if (conferences.length > 0) {
+        spinnerClasses = 'd-flex justify-content-center mb-3 d-none';
+        dropdownClasses = 'form-select';
+        }
 
     return (
         <div className="my-5">
@@ -65,25 +97,25 @@ function AttendeeForm() {
                 <div className="card shadow">
                     <div className="card-body">
                     <form id="create-attendee-form" onSubmit={handleSubmit}>
-                        {conference.length === 0 && (
+                        {/* {conference.length === 0 && (
                             <div className='d-flex justify-content-center mb-3' id="loading-conference-spinner">
                                 <div className='spinner-grow text-secondary' role='status'>
                                     <span className="visually-hidden">Loading...</span>
                                 </div>
                             </div>
-                        )}
+                        )} */}
                         <h1 className="card-title">It's Conference Time!</h1>
                         <p className="mb-3">
                         Please choose which conference
                         you'd like to attend.
                         </p>
-                        <div className="d-flex justify-content-center mb-3" id="loading-conference-spinner">
-                        <div className="spinner-grow text-secondary" role="status">
-                            <span className="visually-hidden">Loading...</span>
-                        </div>
+                        <div className={spinnerClasses} id="loading-conference-spinner">
+                            <div className="spinner-grow text-secondary" role="status">
+                                <span className="visually-hidden">Loading...</span>
+                            </div>
                         </div>
                         <div className="mb-3">
-                        <select onChange={event=>setConference(event.target.value)} value={conference} name="conference" id="conference" className="form-select" required>
+                        <select onChange={handleConferenceChange} value={conference} name="conference" id="conference" className={dropdownClasses} required>
                             <option value="">Choose a conference</option>
                             {conferences.map(conference => (
                                 <option key={conference.href} value={conference.href}>{conference.name}</option>
@@ -96,13 +128,13 @@ function AttendeeForm() {
                         <div className="row">
                         <div className="col">
                             <div className="form-floating mb-3">
-                            <input onChange={event => setName(event.target.value)} value={name} placeholder="Your full name" type="text" id="name" name="name" className="form-control" />
+                            <input onChange={handleNameChange} value={name} placeholder="Your full name" type="text" id="name" name="name" className="form-control" />
                             <label htmlFor="name">Your full name</label>
                             </div>
                         </div>
                         <div className="col">
                             <div className="form-floating mb-3">
-                            <input onChange={event=> setEmail(event.target.value)} value={email} placeholder="Your email address" type="email" id="email" name="email" className="form-control" />
+                            <input onChange={handleEmailChange} value={email} placeholder="Your email address" type="email" id="email" name="email" className="form-control" />
                             <label htmlFor="email">Your email address</label>
                             </div>
                         </div>
